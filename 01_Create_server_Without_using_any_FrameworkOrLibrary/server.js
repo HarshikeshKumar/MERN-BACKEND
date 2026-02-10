0d28e61714ceb7cdc753aa048b703cb16ed3cf71
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-10 12:56:32 +0530

    Abb Server ko pata hai ki server ko start hone pe karna kya hai

--- 01_Create_server_Without_using_any_FrameworkOrLibrary/server.js
+++ 01_Create_server_Without_using_any_FrameworkOrLibrary/server.js
@@ -1,6 +1,18 @@
+// 1.........................
+// const http = require("http");
+
+// const server = http.createServer();
+
+// server.listen(3000, () => {
+//   console.log("Server is running at PORT: 3000");
+// });
+
+// 2...........................
 const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.end("Hello World");
+});
 
 server.listen(3000, () => {
   console.log("Server is running at PORT: 3000");
