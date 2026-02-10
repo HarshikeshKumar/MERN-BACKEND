b5851f43b7d81aab4131258a652e59cd9c57aef6
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-10 13:11:01 +0530

    PRACTICE ON HOW to create server using Node.JS

--- 01_Create_server_Without_using_any_FrameworkOrLibrary/server.js
+++ 01_Create_server_Without_using_any_FrameworkOrLibrary/server.js
@@ -19,17 +19,40 @@
 // });
 
 // 3............................
-const http = require("http");
+// const http = require("http");
 
+// const server = http.createServer((req, res) => {
+//   console.log(req.url);
+//   if (req.url === "/") {
+//     return res.end("Welcome to the home page");
+//   } else {
+//     return res.end("Hello World");
+//   }
+// });
+
+// server.listen(3000, (req, res) => {
+//   console.log("Server is running at PORT:3000");
+// });
+
+// 4.........MULTIPLE ROUTES................
+const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  // console.log(req.url);
   if (req.url === "/") {
-    return res.end("Welcome to the home page");
+    return res.end("Home Page");
+  } else if (req.url === "/about") {
+    return res.end("About Page");
+  } else if (req.url === "/name") {
+    return res.end("Name Page");
+  } else if (req.url === "/gaurav") {
+    return res.end("Gaurav Singh Page");
+  } else if (req.url === "/harshikesh") {
+    return res.end("Harshikesh Kumar Page");
   } else {
-    return res.end("Hello World");
+    return res.end("Route does not Matched");
   }
 });
 
-server.listen(3000, (req, res) => {
-  console.log("Server is running at PORT:3000");
+server.listen(3000, () => {
+  console.log("Server is running at PORT: 3000");
 });
