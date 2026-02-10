c8a8c61f8ac7323c84a3b765e2ae90193f34df19
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-10 16:01:31 +0530

    In this commit, Middleware ka registration hata diya aur Middleware function ko comment kiya, Because ab ham MORGAN Middleware dekhenge

--- 04_TwitterApp/src/index.js
+++ 04_TwitterApp/src/index.js
@@ -2,23 +2,23 @@ import express from "express";
 
 const app = express();
 
-function mid1(req, res, next) {
-  console.log(next);
-  console.log("mid1");
-  next();
-}
+// function mid1(req, res, next) {
+//   console.log(next);
+//   console.log("mid1");
+//   next();
+// }
 
-function mid2(req, res, next) {
-  console.log("mid2");
-  next();
-}
+// function mid2(req, res, next) {
+//   console.log("mid2");
+//   next();
+// }
 
-function mid3(req, res, next) {
-  console.log("mid3");
-  next();
-}
+// function mid3(req, res, next) {
+//   console.log("mid3");
+//   next();
+// }
 
-app.get("/ping", [mid1, mid2, mid3], (req, res) => {
+app.get("/ping", (req, res) => {
   return res.json({
     message: "Pong",
   });
