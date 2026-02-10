28d65be65d37fd4fc276ac52c7e24768c522e264
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-10 13:02:09 +0530

    Abb '/' route pe 'Welcome to home page' milega aur '/kuchhbhi' route pe 'Hello World milega

--- 01_Create_server_Without_using_any_FrameworkOrLibrary/server.js
+++ 01_Create_server_Without_using_any_FrameworkOrLibrary/server.js
@@ -8,12 +8,28 @@
 // });
 
 // 2...........................
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.end("Hello World");
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server is running at PORT: 3000");
+// });
+
+// 3............................
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello World");
+  console.log(req.url);
+  if (req.url === "/") {
+    return res.end("Welcome to the home page");
+  } else {
+    return res.end("Hello World");
+  }
 });
 
-server.listen(3000, () => {
-  console.log("Server is running at PORT: 3000");
+server.listen(3000, (req, res) => {
+  console.log("Server is running at PORT:3000");
 });
