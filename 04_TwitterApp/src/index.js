207d697f86a02a71492b88b3f9a5d3ef220073ed
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-10 15:49:18 +0530

    NOW ADDED 2 API, one is '/ping' and another is '/pong' Routes/API

--- 04_TwitterApp/src/index.js
+++ 04_TwitterApp/src/index.js
@@ -2,6 +2,18 @@ import express from "express";
 
 const app = express();
 
+app.get("/ping", (req, res) => {
+  return res.json({
+    message: "Pong",
+  });
+});
+
+app.get("/hello", (req, res) => {
+  return res.json({
+    message: "World",
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is runnung at PORT: 3000");
 });
