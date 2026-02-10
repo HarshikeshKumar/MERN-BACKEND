56e3790bf2b39b575bd67362de0ab0fd40560ed2
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-10 15:55:26 +0530

    MIDDLEWARES, In this commit create 3 MIDDLEWARES mid1, mid2, and mid3 And register this middlewares in '/ping' route

--- 04_TwitterApp/src/index.js
+++ 04_TwitterApp/src/index.js
@@ -2,7 +2,23 @@ import express from "express";
 
 const app = express();
 
-app.get("/ping", (req, res) => {
+function mid1(req, res, next) {
+  console.log(next);
+  console.log("mid1");
+  next();
+}
+
+function mid2(req, res, next) {
+  console.log("mid2");
+  next();
+}
+
+function mid3(req, res, next) {
+  console.log("mid3");
+  next();
+}
+
+app.get("/ping", mid1, mid2, mid3, (req, res) => {
   return res.json({
     message: "Pong",
   });
