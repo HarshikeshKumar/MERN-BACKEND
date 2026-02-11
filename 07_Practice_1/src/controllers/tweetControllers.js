9d34bfca92450fabafe0c5f2e6b27ac14fb388c0
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-11 22:44:44 +0530

    07.10:--> Implemented 'tweetControllers.js' CONTROLLERS

---
+++ 07_Practice_1/src/controllers/tweetControllers.js
@@ -0,0 +1,25 @@
+export const getV1Tweet = (req, res) => {
+  return res.json({
+    message: "V1 tweet route",
+  });
+};
+
+export const getV1TweetById = (req, res) => {
+  return res.json({
+    message: "V1 tweet route with ID",
+    id: req.params.id,
+  });
+};
+
+export const getV2Tweet = (req, res) => {
+  return res.json({
+    message: "V2 tweet route",
+  });
+};
+
+export const getV2TweetById = (req, res) => {
+  return res.json({
+    message: "V2 tweet route with ID",
+    id: req.params.id,
+  });
+};
