3a2de93fe1510bd0882f390f5a2fc25d5ce5d9fe
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-11 21:54:33 +0530

    07.05:- Added some routes

--- 07_Practice_1/src/index.js
+++ 07_Practice_1/src/index.js
@@ -2,6 +2,18 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Home Page",
+  });
+});
+
+app.all(/.*/, (req, res) => {
+  return res.json({
+    message: "Custom Page not found",
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is running at PORT: 3000");
 });
