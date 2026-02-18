379b80d4964bf834c5aa6ee3dc74370f0813fd9b
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-18 14:00:25 +0530

    09..06, '/api/v2/tweets' and '/api/v2/tweets/:id' API SETUP

---
+++ 09_Practice_3/src/routes/v2/v2TweetRouter.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  return res.json({
+    message: "V2 tweets router",
+  });
+});
+
+router.get("/:id", (req, res) => {
+  return res.json({
+    message: "V2 tweets router with ID",
+    id: req.params.id,
+  });
+});
+
+export default router;
