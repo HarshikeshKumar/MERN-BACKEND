43b182fa59a00635fae399a3a1af78cb7757a581
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-18 13:53:26 +0530

    09..05, In this commit '/api/v1/comments' and '/api/v1/comments/:id' API SETUP

--- 09_Practice_3/src/routes/v1/v1Router.js
+++ 09_Practice_3/src/routes/v1/v1Router.js
@@ -1,8 +1,10 @@
 import express from "express";
 import v1TweetRouter from "./v1TweetRouter.js";
+import v1CommentRouter from "./v1CommentRouter.js";
 
 const router = express.Router();
 
 router.use("/tweets", v1TweetRouter);
+router.use("/comments", v1CommentRouter);
 
 export default router;
