50149ed74dc10730f2fa736f57cb20e3068c4026
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-18 16:28:16 +0530

    09..15, REPOSITORY, Iss commit me maine 'tweetRepository.js' REPOSITORY ko Implement kiya hai

---
+++ 09_Practice_3/src/repository/tweetRepository.js
@@ -0,0 +1,31 @@
+import Tweet from "../../../06_TweeterApp_p_2/src/schema/tweetSchema.js";
+
+// Create Tweet.........
+export const createTweet = async ({ body }) => {
+  try {
+    const tweet = await Tweet.create({ body });
+    return tweet;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Get All Tweet............
+export const getAllTweet = async () => {
+  try {
+    const tweets = await Tweet.find();
+    return tweets;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Get a Tweet.................
+export const getTweetById = async (tweetId) => {
+  try {
+    const tweet = await Tweet.findById(tweetId);
+    return tweet;
+  } catch (error) {
+    throw error;
+  }
+};
