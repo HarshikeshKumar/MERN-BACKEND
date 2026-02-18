76d5c17f5359998064e579b8d320e151929539c0
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-18 14:28:57 +0530

    CONTROLLERS, Implemented 'tweetControllers.js' and register it

--- 09_Practice_3/src/routes/v1/v1TweetRouter.js
+++ 09_Practice_3/src/routes/v1/v1TweetRouter.js
@@ -1,18 +1,13 @@
 import express from "express";
+import {
+  getV1Tweet,
+  getV1TweetById,
+} from "../../controllers/tweetControllers.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  return res.json({
-    message: "V1 tweets Router",
-  });
-});
+router.get("/", getV1Tweet);
 
-router.get("/:id", (req, res) => {
-  return res.json({
-    message: "V1 tweets router with ID",
-    id: req.params.id,
-  });
-});
+router.get("/:id", getV1TweetById);
 
 export default router;
