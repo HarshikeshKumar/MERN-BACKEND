51af392b8254c89a2fd69870d57e80689d63ca84
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-19 16:30:15 +0530

    10..16, 'REPOSITORY' Iss commit me 'tweetSchema.js' ka use krr ke Tweet 'CREATE And READ' krne ka code likha hai

---
+++ 10_TwitterApp_3/src/repository/tweetRepository.js
@@ -0,0 +1,31 @@
+import Tweet from "../schema/tweetSchema.js";
+
+// Create tweet......
+export const createTweet = async ({ body }) => {
+  try {
+    const tweet = await Tweet.create({ body });
+    return tweet;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Read one Tweet.....
+export const getTweet = async (tweetId) => {
+  try {
+    const tweet = await Tweet.findById(tweetId);
+    return tweet;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Read All Tweet.......
+export const getAllTweet = async () => {
+  try {
+    const tweets = await Tweet.find();
+    return tweets;
+  } catch (error) {
+    throw error;
+  }
+};
