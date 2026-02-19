2a7f27d6bb94e7973789b27117a66f6c50475886
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-19 16:50:32 +0530

    10..18, 'REGISTER SERVICE IN CONTROLLERS', Iss commit me maine Services koControllers me REGISTER Kiya

--- 10_TwitterApp_3/src/controllers/tweetController.js
+++ 10_TwitterApp_3/src/controllers/tweetController.js
@@ -1,3 +1,6 @@
+import { success } from "zod";
+import { createTweetService } from "../services/tweetService.js";
+
 export const getV1Tweet = (req, res) => {
   return res.json({
     message: "V1 tweets router",
@@ -24,10 +27,36 @@ export const getV2TweetById = (req, res) => {
   });
 };
 
-export const createTweet = (req, res) => {
-  return res.status(201).json({
-    message: "Tweet Created",
-    success: true,
-    body: req.body,
-  });
+export const createTweet = async (req, res) => {
+  // return res.status(201).json({
+  //   message: "Tweet Created",
+  //   success: true,
+  //   body: req.body,
+  // });
+
+  // REGISTER SERVICE LAYER (creteTweetService) Function Here....
+  try {
+    const response = await createTweetService({
+      body: req.body.body,
+    });
+
+    // agar tweet sahi se create ho gaya....
+    return res.status(201).json({
+      success: true,
+      data: response,
+      message: "Tweet Created Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    if (error.status) {
+      return res.status(error.status).json({
+        message: error.message,
+        success: false,
+      });
+    }
+    return res.status(500).json({
+      message: "Internal Server error",
+      success: false,
+    });
+  }
 };
