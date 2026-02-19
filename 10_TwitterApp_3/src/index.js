3ee9f7d2b1af869783369bcbde00c264a8bdb982
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-19 15:16:42 +0530

    10..10, MIDDLEWARES, Iss commit me kuchh MIDDLEWARES ko pure API pe REGISTER Kiya hai

--- 10_TwitterApp_3/src/index.js
+++ 10_TwitterApp_3/src/index.js
@@ -1,7 +1,14 @@
 import express from "express";
 import apiRouter from "./routes/api/apiRouter.js";
 
+import morgan from "morgan";
+
 const app = express();
+
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded());
 
 app.use("/api", apiRouter);
 
