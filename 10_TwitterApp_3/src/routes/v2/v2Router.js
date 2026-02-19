e1b54058c696e2f78fb84e7e9aeeb6f37a4bfdf1
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-02-19 14:49:46 +0530

    10..7, ROUTING, '/api/v2/comments' aur '/api/v2/comments/:id' API ko Implement kiy gya

--- 10_TwitterApp_3/src/routes/v2/v2Router.js
+++ 10_TwitterApp_3/src/routes/v2/v2Router.js
@@ -1,8 +1,10 @@
 import express from "express";
 import v2TweetRouter from "./v2TweetRouter.js";
+import v2CommentRouter from "./v2CommentRouter.js";
 
 const router = express.Router();
 
 router.use("/tweets", v2TweetRouter);
+router.use("/comments", v2CommentRouter);
 
 export default router;
